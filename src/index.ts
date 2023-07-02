3db6cab44103972a72e85cdbaa5f1b177776d018
Dipu <dipuchaurasiya91@gmail.com>
2023-07-02 23:42:36 +0545

    server added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import errorHandler from "./utils/errorHandle";
+import userRoutes from "./routes/userRoutes";
 const app = express();
+app.use(express.json());
+
+app.use("/user", userRoutes);
 
 const port = 3000;
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Server listening on port " + port);
