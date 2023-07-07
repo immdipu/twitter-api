90e08d87884d6e74b1a8f65873861efd1437af89
Dipu <dipuchaurasiya91@gmail.com>
2023-07-07 10:12:24 +0545

    retweet bug fixed

--- src/types/postTypes.ts
+++ src/types/postTypes.ts
@@ -4,7 +4,7 @@ export interface PostSchemaTypes extends Document {
   content: string;
   postedBy: Schema.Types.ObjectId;
   pinned?: boolean;
-  type: "tweet" | "reply" | "reweet";
+  type: "tweet" | "reply" | "retweet";
   likes?: Schema.Types.ObjectId[];
   retweetUsers?: Schema.Types.ObjectId[];
   retweetData?: Schema.Types.ObjectId;
